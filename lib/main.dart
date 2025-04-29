5326ed6636c662436fbc3c45ca3e28c2a687aa6e
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2025-04-29 23:40:49 +0300

    added image_picker

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/login',
       routes: {
         '/login': (context) => Login(),
         '/home': (context) => Home(),
